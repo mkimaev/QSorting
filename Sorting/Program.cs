ad5450b148781bc543346f5efbbff7eb62ae5139
mkimaev <maxim.kimaev@gmail.com>
2016-04-18 14:23:24 +0300

    Change params

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -9,38 +9,41 @@ namespace Sorting
     class Program
     {
         //метод с тремя вводными параметрами (название массива, первый индекс, последний индекс)
-        static void quickSort(int[] a, int l, int r)
+        static void QuickSort(int[] array, int first, int last)
         {
             int temp; //переменная для хранения временных значений
-            int x = a[l + (r - l) / 2]; //переменная для опорного индекса
-            int i = l; //переменная для начального индекса
-            int j = r; //переменная для последнего индекса
+            int x = array[first + (last - first) / 2]; //переменная для опорного индекса
+            int i = first; //переменная для начального индекса
+            int j = last; //переменная для последнего индекса
             
             while (i <= j) //пробегаемся по массиву
             {
-                while (a[i] < x) i++; //пропускаем если левые меньше опорного
-                while (a[j] > x) j--; //пропускаем если правые больше опорного
+                while (array[i] < x) i++; //пропускаем если левые меньше опорного
+                while (array[j] > x) j--; //пропускаем если правые больше опорного
                 if (i <= j) //если есть нераскиданные значения - переставляем их местами
                 {
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                     i++;
                     j--;
                 }
             }
-            if (i < r) //вызываем сортировку рекурсивно для левого подмассива
-                quickSort(a, i, r);
-
-            if (l < j) //вызываем сортировку рекурсивно для правого подмассива
-                quickSort(a, l, j);
+            if (i < last) //вызываем сортировку рекурсивно для левого подмассива
+            {
+                QuickSort(array, i, last);
+            }
+            if (first < j) //вызываем сортировку рекурсивно для правого подмассива
+            {
+                QuickSort(array, first, j);
+            }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Привет, Бро!\nВведи размер массива[]");
             int size;
             size = Convert.ToInt32(Console.ReadLine()); // ввод
-            Console.WriteLine("Введите " + size + " чисел через ЗАПЯТУЮ и нажми Enter "); 
+            Console.WriteLine("Введите " + size + " чисел через ЗАПЯТУЮ и нажмите Enter "); 
             Console.WriteLine();
             string str = Console.ReadLine();
             string[] mas = str.Split(','); //применяем класс который делит строчку на части между символами. у нас через запятую
@@ -51,7 +54,7 @@ namespace Sorting
             {
                 a[i] = int.Parse(mas[i]); // парсим строчные в int
             }
-            quickSort(a, 0, size - 1); //вызываем метод быстрой сортировки массива "а" с нулевого по последний елемент массива
+            QuickSort(a, 0, size - 1); //вызываем метод быстрой сортировки массива "а" с нулевого по последний елемент массива
             for (int i = 0; i < size; i++) //запускаем цикл для отображения сортировки методом быстрой сортировки
             {
                 Console.WriteLine(a[i]);
